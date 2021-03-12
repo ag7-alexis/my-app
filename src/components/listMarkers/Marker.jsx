f1e0e97ed56a76520b1d6cd41a96fc70bbd331aa
ag7 <alexis26999@gmail.com>
2021-03-12 17:01:42 +0100

    finito

--- src/components/listMarkers/Marker.jsx
+++ src/components/listMarkers/Marker.jsx
@@ -5,7 +5,7 @@ const Marker = ({ marker, index }) => {
   const { dispatch } = useMarkerContext();
   return (
     <li key={`${index}-marker`}>
-      {index} {marker[0]} {marker[1]}
+      {index}
       <input
         value={marker[2]}
         onChange={(event) =>
